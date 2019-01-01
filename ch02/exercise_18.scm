ffa2131d1d878b2d5f07f90b6ef987f6ea48e06f
Edward Kim <ekim@agero.com>
2019-01-01 15:36:49 -0500

    add 2.18

---
+++ ch02/exercise_18.scm
@@ -0,0 +1,9 @@
+; Exercise 2.18: Define a procedure reverse that takes a list as argument and
+; returns a list of the same elements in reverse order:
+
+(define (reverse items)
+  (if (null? items)
+      items
+      (append (reverse (cdr items)) (list (car items)))))
+
+(reverse (list 1 4 9 16 25))
