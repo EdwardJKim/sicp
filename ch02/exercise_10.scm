9224e642dc15c8724ff2bc70ca855ae75c0ba9dc
Edward Kim <ekim@agero.com>
2019-01-01 11:42:02 -0500

    add 2.10

---
+++ ch02/exercise_10.scm
@@ -0,0 +1,20 @@
+; Exercise 2.10: Ben Bitdiddle, an expert systems programmer, looks over
+; Alyssa’s shoulder and comments that it is not clear what it means to divide
+; by an interval that spans zero. Modify Alyssa’s code to check for this
+; condition and to signal an error if it occurs.
+
+(load "exercise_09.scm")
+
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval
+                 (/ 1.0 (upper-bound y))
+                 (/ 1.0 (lower-bound y)))))
+
+(define (div-interval x y)
+  (if (or (= (lower-bound y) 0) (= (upper-bound y) 0))
+      (error "Division by zero: CONS" (lower-bound y) (upper-bound y))
+      (mul-interval x
+                    (make-interval
+                    (/ 1.0 (upper-bound y))
+                    (/ 1.0 (lower-bound y))))))
