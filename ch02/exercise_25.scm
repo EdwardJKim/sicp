438def3fc4cf9e39f573a41bb7aa224b845d4d47
Edward Kim <ekim@agero.com>
2019-01-03 14:45:23 -0500

    add 2.25

---
+++ ch02/exercise_25.scm
@@ -0,0 +1,15 @@
+; Exercise 2.25: Give combinations of cars and cdrs that will pick 7 from each
+; of the following lists:
+;
+; (1 3 (5 7) 9)
+; ((7))
+; (1 (2 (3 (4 (5 (6 7))))))
+
+(define x '(1 3 (5 7) 9))
+(car (cdr (car (cdr (cdr x)))))
+
+(define y '((7)))
+(car (car y))
+
+(define z '(1 (2 (3 (4 (5 (6 7)))))))
+(cadr (cadr (cadr (cadr (cadr (cadr z))))))
