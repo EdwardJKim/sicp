0b9f1b9d66fd008d65c65cdabe803c8e954750fb
Edward Kim <ekim@agero.com>
2019-01-04 16:07:06 -0500

    add 2.40

--- ch02/ch02.scm
+++ ch02/ch02.scm
@@ -1,3 +1,5 @@
+(load "../ch01/ch01.scm")
+
 ;; Section 2.1
 
 (define (add-rat x y)
@@ -63,3 +65,49 @@
           (accumulate op
                       initial
                       (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low
+            (enumerate-interval
+             (+ low 1)
+             high))))
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair)
+        (cadr pair)
+        (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter
+        prime-sum?
+        (flatmap
+         (lambda (i)
+           (map (lambda (j)
+                  (list i j))
+                (enumerate-interval
+                 1
+                 (- i 1))))
+         (enumerate-interval 1 n)))))
+
+(define (permutations s)
+  (if (null? s)
+      (list '())
+      (flatmap (lambda (x)
+                 (map (lambda (p)
+                        (cons x p))
+                      (permutations
+                       (remove x s))))
+               s)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+          sequence))
