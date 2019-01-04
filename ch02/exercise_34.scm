c3c02a14083d624fd2eb6d2b345632fb76ec2d54
Edward Kim <ekim@agero.com>
2019-01-04 14:38:57 -0500

    fix 2.34

--- ch02/exercise_34.scm
+++ ch02/exercise_34.scm
@@ -14,6 +14,17 @@
 ; Fill in the following template to produce a procedure that evaluates a
 ; polynomial using Horner’s rule. Assume that the coefficients of the
 ; polynomial are arranged in a sequence, from a_0 through a_n.
+;
+; (define (horner-eval x coefficient-sequence)
+;   (accumulate
+;     (lambda (this-coeff higher-terms)
+;       <??>)
+;     0
+;     coefficient-sequence))
+;
+; For example, to compute 1 + 3x + 5x^3 + x^5 at x = 2 you would evaluate
+;
+; (horner-eval 2 (list 1 3 0 5 0 1))
 
 (load "ch02.scm")
 
@@ -23,3 +34,5 @@
       (+ this-coeff (* x higher-terms)))
     0
     coefficient-sequence))
+
+(horner-eval 2 (list 1 3 0 5 0 1))
