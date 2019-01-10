df14e31b7eafda5b5a1173451bf0ebdf44b54d9c
Edward Kim <ekim@agero.com>
2019-01-10 14:02:24 -0500

    add 2.44

---
+++ ch02/exercise_44.scm
@@ -0,0 +1,10 @@
+; Figure 2.14: The recursive operations right-split and corner-split applied to
+; the painters wave and rogers. Combining four corner-split figures produces
+; symmetric square-limit designs as shown in Figure 2.9.
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below (beside smaller smaller)
+               painter))))
